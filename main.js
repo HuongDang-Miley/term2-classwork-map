b3421cbcb74c57a4b2e82a96e0e745157a234bfa
Huong Dang <miley.dawn@codeimmersives.com>
2020-09-15 14:29:36 -0400

    classwork

---
+++ main.js
@@ -0,0 +1,57 @@
+// 1.  Map through the array and console.log everyone's first and last name. Using .map
+let users = [
+    {
+    firstName: 'Joe',
+    lastName:'Orin',
+    car: 'Porsche',
+    dog:'Shitzu',
+    },
+    {
+    firstName: 'Lori',
+    lastName:"Foster",
+    car: 'Porsche',
+    dog:'German Shepherd'
+    },
+    {
+    firstName: 'Guy',
+    lastName:'Peters',
+    car: 'Chevrolet',
+    dog:'Poodle'
+    },
+    {
+    firstName: 'Lori',
+    lastName:'Foster',
+    car: 'Toyota',
+    dog:'German Shepard'
+    }
+    ];
+    
+    let fullNames = users.map((person) => `${person.firstName} ${person.lastName}`)
+    console.log(fullNames)
+
+
+// 2.  a. Create an array of objects where the value from the name key in each object in users is the key in the new object index and the likes value is the value in the new object index.
+//     b. Calculate age based on the number of letters in the name multiplied by 10
+
+    const users2 = [
+    { name: 'pan', likes: 'lasagne' },
+    { name: 'lena', likes: 'wine' },
+    { name: 'jill', likes: 'tacos' },
+    { name: 'conner', likes: 'turkey' }
+    ];
+//     Should look like:
+//     [
+//     { pan: 'lasagne', age: 30 },
+//     { lena: 'wine', age: 40 },
+//     { jill: 'tacos', age: 40 },
+//     { conner: 'turkey', age: 60 }
+//     ]
+
+let result = users2.map((person) => {
+    person.name = person.likes
+    person['age'] = person.likes.length
+    delete person.likes
+    return person   
+}, {})
+
+console.log(result)
